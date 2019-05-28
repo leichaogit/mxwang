a74f1cbf7ebb4f98fdbeb3329124db8677f25842
leichao <15827494582@163.com>
2019-05-28 20:28:17 +0800

    慕萌

--- apps/user/adminx.py
+++ apps/user/adminx.py
@@ -1,5 +1,17 @@
 import xadmin
 from .models import EmailVerifyRecord, Banner
+from xadmin import views
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSetting(object):
+    site_title = '慕萌后台管理'
+    site_footer = '慕萌学习网'
+    menu_style = 'accordion'
 
 
 class EmailVerifyRecordAdmin(object):
@@ -16,3 +28,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)
